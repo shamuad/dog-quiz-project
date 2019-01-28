69789503fe670f65dced77f76c7bdc967f544598
“ALEX <alexgtr84@gmail.com>
2019-01-28 15:41:30 +0100

    Add component and component container

---
+++ src/components/QuestionContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import Question from './Question';
+
+class QuestionContainer extends Component {
+    status = {
+
+    }
+
+    render() {
+
+      return
+
+    }
+  }
+  
+  const mapStateToProps = state => {
+    return {
+      
+    }
+  }
+  
+  export default (App)
+
+//   connect(mapStateToProps)
