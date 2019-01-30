764cdee680dc51a336e64de657725e8d1af788a0
CHARLES <charlescallaghan@gmail.com>
2019-01-30 15:49:55 +0100

    Added display functionality and working questions

--- src/components/QuizContainer.js
+++ src/components/QuizContainer.js
@@ -4,33 +4,88 @@ import { connect } from 'react-redux'
 import { buttonIncrement } from '../actions/buttoncounter'
 
 
+
+
+let imageNumber = Math.floor((Math.random() * 10) + 1);
+let randomNums = []
+// const otherOptions = if(imageNumber = otherOptions)
+
 class QuizContainer extends Component {
 
     incrementCounter = () => {
         this.props.buttonIncrement()
     }
 
+    // randomNum = (min, max) => {
+    //     for (var i = 0; i < 3; i++) {
+    //         randomNums.push(Math.floor(Math.random() * max) + min);
+    //     }
+    //     return randomNums;
+    // }
+
+
     render() {
+        if (this.props.breeds.length < 87) return 'Testing...'
+        // console.log(this.chooseMines(3))
         return (<div>
-            <div className="Question">
-            <h1>What is this breed?</h1>
-        </div>
-            <div className="Answer">
-                <Quiz content='Answer 1' test={this.incrementCounter} />
-                <Quiz content='Answer 2' test={this.incrementCounter} />
-                <Quiz content='Answer 3' test={this.incrementCounter} />
-            </div>
-            </div >)
+
+            <img className="dogImage" key={this.props.breeds[randomNums[2]].image} src={this.props.breeds[randomNums[2]].image} alt="Dog" className="Image"></img>
+
+            <h1>What breed is this?</h1>
+
+            <Quiz content={this.props.breeds[randomNums[2]].breed} test={this.incrementCounter} />
+            <Quiz content={this.props.breeds[randomNums[0]].breed} test={this.incrementCounter} />
+            <Quiz content={this.props.breeds[randomNums[1]].breed} test={this.incrementCounter} />
+
+        </div>)
+
     }
 }
+
+console.log(randomNums)
 
 
 const mapStateToProps = (state) => {
     return {
         numClicked: state.numClicked,
+        breeds: state.breeds,
         state
     }
 }
 
 
 export default connect(mapStateToProps, { buttonIncrement })(QuizContainer)
+
+        // const { dogBreeds } = this.props
+        // // console.log(this.props)
+
+        // return (<div>
+        //     {/* {!dogBreeds && 'Loading...'}
+
+        //     {console.log(this.props.breeds)} */}
+
+
+        //     <div>
+        //         <div className="Question">
+        //             <h1>What is this breed?</h1>
+        //         </div>
+        //         <div className="Answer">
+        //             <Quiz content={ this.props.breeds[2] } test={this.incrementCounter} />
+        //             {/* <Quiz content='Answer 2' test={this.incrementCounter} />
+        //             <Quiz content='Answer 3' test={this.incrementCounter} /> */}
+        //         </div>
+        //     </div >
+        // </div>)
+
+
+    // chooseMines = (num) => {
+    //     var num = num || 100;
+    //     var currentNum = 0;
+    //     for (var i = 0; i < 10; i++) {
+    //         do {
+    //             currentNum = Math.ceil(Math.random() * num);
+    //         } while(randomNums.includes(currentNum))
+    //         randomNums.push(currentNum);
+    //     }
+    //     return randomNums;
+    // }
