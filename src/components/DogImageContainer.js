bd01320bded099a08bb6766c6449a62b6e689724
“ALEX <alexgtr84@gmail.com>
2019-02-01 13:38:40 +0100

    Add final css and code clean up

--- src/components/DogImageContainer.js
+++ src/components/DogImageContainer.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-
 import * as request from 'superagent'
 import { SetDogBreed } from '../actions/setDogBreed'
 import { connect } from 'react-redux'
 
 var shuffle = require('shuffle-array')
 
-const randomImage = shuffle.pick([1,2,3,4,5])
+const randomImage = shuffle.pick([1, 2, 3, 4, 5])
 
 class DogsImagesContainer extends Component {
     state = {
@@ -23,23 +22,20 @@ class DogsImagesContainer extends Component {
                         .then(response => {
                             this.props.SetDogBreed(breed, response.body.message[randomImage])
                         })
-
                 })
-
-
             })
             .catch("error")
-
-
     }
     render() {
         return (<div className="img" > { /* {this.state.dogBreeds} */} </div>
         )
     }
 }
+
 const mapStateToProps = (state) => {
     return {
         state
     }
 }
+
 export default connect(mapStateToProps, { SetDogBreed })(DogsImagesContainer)
